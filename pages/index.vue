ae0db937368ac37784549afb2ffb731e0d534015
KodjoCode <appohfaiths@gmail.com>
2024-05-12 00:34:44 +0000

    Project setup

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+
+const { data: charmander} = await useFetch('https://pokeapi.co/api/v2/pokemon/charmander')
+</script>
+
+<template>
+  <div>
+    Home Page for Nuxt 3 server course
+  </div>
+  <pre>
+    {{ charmander }}
+  </pre>
+</template>
+
+<style scoped></style>
